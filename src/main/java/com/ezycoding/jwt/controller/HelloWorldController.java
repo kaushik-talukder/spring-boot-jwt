99b985ad7afadbf4d6c10a700cfbc8738d99926c
Kaushik Talukder <kaushik.talukder@gmail.com>
2020-07-05 17:23:11 -0400

    Change user name and pwd

--- src/main/java/com/ezycoding/jwt/controller/HelloWorldController.java
+++ src/main/java/com/ezycoding/jwt/controller/HelloWorldController.java
@@ -2,13 +2,14 @@ package com.ezycoding.jwt.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin()
 public class HelloWorldController {
 
-	@RequestMapping({ "/hello" })
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
 		return "Hello World";
 	}
